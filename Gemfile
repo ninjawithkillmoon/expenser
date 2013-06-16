a92578c7a93475078bed059fc1a9d5eede8027bb
ninjawithkillmoon <john.william.tonkin@gmail.com>
2013-06-16 13:25:53 +0930

    initial commit

---
+++ Gemfile
@@ -0,0 +1,18 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+
+group :development do
+  gem 'sqlite3'
+  gem 'annotate'
+end
+
+group :production do
+  gem 'pg'
+end
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'uglifier'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NwkmExpenser::Application.config.secret_token = 'a9668a8ea3ad31f2e36992bb19fa21f81a64b87e8f139a6857471e92a3273b63975d44b9fe7253907aa6d1a06bd0cd96cb155f2138af58b42c7a2ca55ac9a8af'
